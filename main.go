b44d1918aea877b729ede08b408ebaa84ee6870f
Jose Orlicki <joigno@gmail.com>
2019-08-30 17:52:16 +0200

    Command line arguments for alien invasion.

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"AlienInvasion/invasion"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 
 func main() {
 
+	if len(os.Args[1:]) < 3 {
+		fmt.Println("Usage: %s inputfile.map alienPopulation numberOfSteps [-s]   (-s for synchronic invasion, not asynchornic)", os.Args[0])
+		os.Exit(0)
+	}
 
-	mainInvasion := invasion.New("maps/big.map", 16)
-	//mainInvasion.RunInvasionSync( 16 )
-	mainInvasion.RunInvasionAsync(20)
-	mainInvasion.GetAliens().NumberOfAliensAlive()
+	// alienPopulation
+	i2, _ := strconv.Atoi(os.Args[2])
+	mainInvasion := invasion.New(os.Args[1], i2)
+
+	// numberOfSteps
+	i3, _ := strconv.Atoi(os.Args[3])
+
+	// Use sync version or not
+	syncVersion := len(os.Args[1:]) > 3 && os.Args[len(os.Args)-1]  == "-s"
+	if syncVersion {
+		mainInvasion.RunInvasionSync( i3 )
+	} else {
+		mainInvasion.RunInvasionAsync(i3)
+	}
+
 }
